cc9bb58a44e06833f14a1f900ea6dd0c4cff9785
Sony K <sony.kusyana@gmail.com>
2018-10-11 09:40:09 +0700

    use criteria

--- src/Adapters/ModelAdapter.php
+++ src/Adapters/ModelAdapter.php
@@ -2,39 +2,36 @@
 
 namespace DataTables\Adapters;
 
-use Phalcon\Mvc\Model\Criteria;
-
 class ModelAdapter extends \DataTables\DataTable
 {
     private $criteria;
 
-    public function __construct($model, $criteria = null)
+    public function __construct($model, $params = null)
     {
         parent::__construct();
 
-        $this->criteria = !is_null($criteria) ? $criteria : new Criteria();
-        $this->criteria->setModelName($model);
-
-        $this->total = $model::count($this->criteria->getParams());
+        $builder = is_string($model) ? $model::query() : $model;
+        
+        $this->total = count($builder->execute());
         
         if (empty($this->search['value']))
         {
             foreach ($this->columns as $column){
                 if (!empty($column['search']['value'])){
-                    $this->criteria->andWhere($column['search']['data'] . " LIKE '%" . $column['search']['value'] . "%'");
+                    $builder->andWhere($column['data'] . " LIKE '%" . $column['search']['value'] . "%'");
                 }
             }
         } else {
             foreach ($this->columns as $column){
-                $this->criteria->orWhere($column['search']['data'] . " LIKE '%" . $this->search['value'] . "%'");
+                $builder->orWhere($column['data'] . " LIKE '%" . $this->search['value'] . "%'");
             }
         }
+        
+        $this->filtered = count($builder->execute());
 
-        $this->filtered = $model::count($this->criteria->getParams());
+        $builder->limit($this->limit, $this->offset);
+        $builder->orderBy($this->columns[$this->order[0]['column']]['data'] . ' ' . $this->order[0]['dir']);
 
-        $this->criteria->limit($this->limit, $this->offset);
-        $this->criteria->orderBy($this->order[0]['column'], $this->order[0]['dir']);
-
-        $this->records = $model::find($this->params);
+        $this->records = $builder->execute();
     }
 }
